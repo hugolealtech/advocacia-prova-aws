1b18ca70b36a1c0387952d9f5f47a22c4745ddba
hugolealtech <76668742+hugolealtech@users.noreply.github.com>
2024-12-04 18:13:39 -0300

    primeira pagina em andamento

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { CarousselComponent } from './components/caroussel/caroussel.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component'
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet, 
+    AboutUsComponent, 
+    CarousselComponent, 
+    ContactFormComponent, 
+    HeaderComponent, 
+    MatSlideToggleModule,
+    HomeComponent,
+    FooterComponent,
+    
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'prova-advocacia-aws';
+}
